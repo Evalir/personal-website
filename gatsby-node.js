15c2c65318c7920b9c0ce80f1f7678343af6d164
Enrique Ortiz <hi@enriqueortiz.dev>
2021-02-23 18:52:43 -0400

    feat: add basic rss generation

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,6 +1,19 @@
 const path = require('path')
+const { createFilePath } = require(`gatsby-source-filesystem`)
 
 const POSTS_PER_PAGE = 10
+
+exports.onCreateNode = ({ node, actions, getNode }) => {
+  const { createNodeField } = actions
+  if (node.internal.type === `MarkdownRemark`) {
+    const value = createFilePath({ node, getNode })
+    createNodeField({
+      name: `slug`,
+      node,
+      value,
+    })
+  }
+}
 
 exports.createPages = async ({ actions, graphql, reporter }) => {
   const { createPage } = actions
