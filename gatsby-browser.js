7b23bf2dd54da7520de816b2564c8422d7f4301b
Enrique Ortiz <hi@enriqueortiz.dev>
2021-02-27 13:56:44 -0400

    feat: prism for code snippets

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,6 +2,7 @@ import React from 'react'
 import 'normalize.css'
 import '@fontsource/source-sans-pro'
 import Layout from './src/components/layout/layout'
+import './src/lib/prism.css'
 
 export function wrapPageElement({ element, props }) {
   return <Layout {...props}>{element}</Layout>
--- gatsby-config.js
+++ gatsby-config.js
@@ -107,6 +107,36 @@ module.exports = {
           {
             resolve: 'gatsby-remark-external-links',
           },
+          {
+            resolve: `gatsby-remark-prismjs`,
+            options: {
+              classPrefix: 'language-',
+              inlineCodeMarker: null,
+              aliases: {},
+              showLineNumbers: false,
+              noInlineHighlight: false,
+              languageExtensions: [
+                {
+                  language: 'superscript',
+                  extend: 'javascript',
+                  definition: {
+                    superscript_types: /(SuperType)/,
+                  },
+                  insertBefore: {
+                    function: {
+                      superscript_keywords: /(superif|superelse)/,
+                    },
+                  },
+                },
+              ],
+              prompt: {
+                user: 'root',
+                host: 'localhost',
+                global: false,
+              },
+              escapeEntities: {},
+            },
+          },
         ],
       },
     },
